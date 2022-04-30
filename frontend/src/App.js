3dfbfa939f63adc8b219cec59748d4c3451e74e1
dcarroll9398 <dcarroll9398@yahoo.com>
2022-04-30 11:29:30 -0500

    Video-03-Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <div>
+          <a href="/">amazona</a>
+        </div>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
